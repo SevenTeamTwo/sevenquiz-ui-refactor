e8decf63e5f47b93b35f4ae16b8c7bcb0c83617a
Adam Brutsaert <brutsaertadam@yahoo.fr>
2025-01-24 23:01:14 +0100

    feat: focusing input

--- app/lobby/states/playing-text.tsx
+++ app/lobby/states/playing-text.tsx
@@ -2,6 +2,7 @@ import { Timer } from "~/lobby/components/timer";
 import { Input } from "~/shadcn/components/input";
 
 import { useLobbyActor } from "~/lobby/hooks/actor";
+import { useEffect, useRef } from "react";
 
 export interface PlayingTextProps {
   title: string;
@@ -10,6 +11,13 @@ export interface PlayingTextProps {
 
 export function PlayingText(props: PlayingTextProps) {
   const actor = useLobbyActor();
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref !== null && ref.current !== null) {
+      ref.current.focus();
+    }
+  }, []);
 
   return (
     <div className="flex-grow grid grid-rows-[auto,3fr,7fr] place-items-center gap-2 md:gap-6 p-6">
@@ -18,6 +26,7 @@ export function PlayingText(props: PlayingTextProps) {
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight">{props.title}</h2>
       </div>
       <Input
+        ref={ref}
         placeholder="Entrez votre réponse ici"
         className="max-w-xl px-4 py-6 text-lg md:text-lg"
         onChange={(event) => actor.send({ type: "actionAnswerText", answer: event.target.value })}
